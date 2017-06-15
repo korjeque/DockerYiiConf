66072e40341272514bb7672c1aba9511eab5c5b6
Бакулин Михаил <bakulin_m@tass.ru>
2017-06-15 18:08:09 +0300

    Nginx and Postgres

---
+++ 2/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PG_PASSWORD=${PG_PASSWORD:-$(pwgen -c -n -1 16)}
+
+if [ ! -d "$PG_DATA" ]; then
+
+    echo "${PG_PASSWORD}" > ${PG_PASSWORD_FILE}
+    chmod 600 ${PG_PASSWORD_FILE}
+
+    ${PG_BINDIR}/initdb --locale ${LANG} --pgdata=${PG_DATA} --pwfile=${PG_PASSWORD_FILE} \
+        --username=postgres --encoding=UTF8 --auth=trust
+
+    echo "*************************************************************************"
+    echo " PostgreSQL password is ${PG_PASSWORD}"
+    echo "*************************************************************************"
+
+    unset PG_PASSWORD
+fi
+
+exec "$@"
